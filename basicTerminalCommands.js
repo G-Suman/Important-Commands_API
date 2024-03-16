3269ebbfbab8e007f2ac3aa0f752cca0b3579157
Suman.G <0107sumangiri@gmail.com>
2024-03-16 08:19:13 +0545

    Updated basicTerminalCommands.js

--- basicTerminalCommands.js
+++ basicTerminalCommands.js
@@ -1,17 +1,30 @@
 Pwd : print working directory ,
+    
 cd : change directory ,
+    
 mkdir : create new directory .
+    
 ls : list the file and folder of the directory.
+    
 touch : create an empty file
-icat : see the contents of the file.
+
+cat : see the contents of the file.
+    
 vi : edit the content of the file : esc - I - esc - : wq (save and exit) and : q! (exit without saving).
+    
 mv : move file and folder into the another folder , for moving file : mv filename space foldername / ,
 for moving folder : mv foldername space destination foldername , or , relative path to : cd desktop/foldername space foldername.
+    
 cp : copy the file into the another folder ,
+    
 for file : cp foldername/filename space destination foldername / ,
 copy the empty folder into the another folder : cp -r foldername space destinaton foldername
+
 10 : clear : clear the terminal
+
 11 : relative path : desktop/foldername
+
 12 : cd.. : one folder back.
+    
 13 : cd .. / .. : two folder back .
  - The End - 
