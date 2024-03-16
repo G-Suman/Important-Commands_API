f2daae818277e5133b66c1598a3d8aa1b7f9aa69
Suman.G <kit22e.03sum@ismt.edu.np>
2024-03-16 16:21:35 +0545

    Updated _TerminalCommands.js

--- BasicsCommand_Api/_TerminalCommands.js
+++ BasicsCommand_Api/_TerminalCommands.js
@@ -12,13 +12,18 @@ cat : see the contents of the file.
     
 vi : edit the content of the file : esc - I - esc - : wq (save and exit) and : q! (exit without saving).
     
-mv : move file and folder into the another folder , for moving file : mv filename space foldername / ,
+mv : move file and folder into the another folder , for moving file : mv filename space foldername / eg : mv hey.txt ../practice2/,
+eg : mv practice/hey.txt practice2
+
 for moving folder : mv foldername space destination foldername , or , relative path to : cd desktop/foldername space foldername.
+eg: ../practice2 ~/Desktop/practice/
     
 cp : copy the file into the another folder ,
     
 for file : cp foldername/filename space destination foldername / ,
-copy the empty folder into the another folder : cp -r foldername space destinaton foldername
+eg : cp okk ../practice2
+copy the  folder into the another folder : cp -r foldername space destinaton foldername
+eg : cp -r ~/desktop/practice ~/desktop/practice2/
 
 10 : clear : clear the terminal
 
