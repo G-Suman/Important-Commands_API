fc16c6146751da69fa97397df232d9fd144950df
Suman.G <0107sumangiri@gmail.com>
2024-03-19 22:43:42 +0545

    git command added

---
+++ Basics Github Command/index.js
@@ -0,0 +1,27 @@
+- git init 
+- git status
+- ls -a
+- rm -rf filename
+- git add . 
+- git add filename
+- git commit -m "message"
+- git restore -- staged filename 
+- git log
+- git checkout branchname 
+- git checkout -b branchname 
+- git remote -v
+- git ls - remote
+- git pull origin main
+- git branch -d branchname
+- git reset id 
+- git add filename+ git stash filename
+- git stash pop
+- git stash clear
+- git remote add origin url 
+- git push origin branch name 
+- ls .git
+- git merge branchname 
+- git clone .
+- git remote add upstream url 
+- git branch
+
