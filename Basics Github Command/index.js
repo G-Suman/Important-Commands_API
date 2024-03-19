8dfc975f9a0f4a69d194899c6d9072a94c11ec43
Suman.G <0107sumangiri@gmail.com>
2024-03-19 23:16:07 +0545

    added new commands

--- Basics Github Command/index.js
+++ Basics Github Command/index.js
@@ -1,3 +1,4 @@
+
 - git init 
 - git status
 - ls -a
@@ -24,4 +25,43 @@
 - git clone .
 - git remote add upstream url 
 - git branch
+- git merge main
+- git diff 
+- git commit -am
+- git rm --cached filename
+- git commit -m "message " - m "message "
+- git fetch
 
+
+git init
+git status
+ls -a
+rm -rf filename
+git add .
+git add filename
+git commit -m “message”
+git restore – staged filename
+git log
+git checkout branchname
+git checkout -b branchname
+git remote -v
+git ls - remote
+git pull origin main
+git branch -d branchname
+git reset id
+git add filename+ git stash filename
+git stash pop
+git stash clear
+git remote add origin url
+git push origin branch name
+ls .git
+git merge branchname
+git clone .
+git remote add upstream url
+git branch
+git merge main
+git diff
+git commit -am
+git rm --cached filename
+git commit -m "message " - m "message "
+git fetch
